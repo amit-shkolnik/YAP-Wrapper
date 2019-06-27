feff56e27e2eb9a325fb623750c9782c152ba72f
amit.shkolnik <amit.shkolnik@gmail.com>
2019-06-27 14:59:16 +0300

    On going

--- yap_api.py
+++ yap_api.py
@@ -18,6 +18,7 @@
 ##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import pandas as pd
+import numpy as np
 import requests
 import sys
 import traceback
@@ -25,6 +26,8 @@ import csv
 import re, string
 from enums import *
 from hebtokenizer import HebTokenizer
+
+
 
 class YapApi(object):
     """
@@ -331,11 +334,17 @@ if __name__ == '__main__':
     היא לא יודעת, מה עובר עליי. \
     אמנם אין אהבה שאין לה סוף אבל הסוף הזה נראה לי מקולל הולך בין האנשים ברחוב צועק או או או או או או \
     תגידו לה."   
+    
     # IP of YAP server, if locally installed then '127.0.0.1'
     ip='127.0.0.1:8000'
     yap=YapApi()    
-    tokenized_text, segmented_text, lemmas, dep_tree, md_lattice, ma_lattice=yap.run(text, ip)             
+    tokenized_text, segmented_text, lemmas, dep_tree, md_lattice, ma_lattice=yap.run(text, ip)                 
+    print(tokenized_text)
+    print(segmented_text)
     print(lemmas)
+    print(dep_tree.to_string())
+    print(md_lattice)
+    print(ma_lattice)
     print('Program end')
         
     
