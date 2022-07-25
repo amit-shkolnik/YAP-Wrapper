ee94233be8764073c175e1251b898eef3df8654b
shovalsa <shovatz@gmail.com>
2022-07-25 18:18:59 +0300

    change edges

--- spmrl_to_ud.py
+++ spmrl_to_ud.py
@@ -13,11 +13,43 @@ FEATURES = CONVERSION_TABLE["basic_features"]
 ENTIRE_LINE = CONVERSION_TABLE["entire_line_pos_conversion"]
 
 
+def reorganize_conjunction(graph, node_idx):
+    node = graph.nodes[node_idx]
+    top_label = node["pos"]["old"]
+    children = [n for n in graph.nodes if graph.nodes[n]["parent"] == node_idx]
+    min_node = graph.nodes[children[0]]
+    min_node["label"]["new"] = top_label
+
+
+def reverse_arc_direction(graph, node_idx, new_head_label):
+    node = graph.nodes[node_idx]
+    parent = list(graph.predecessors(node_idx))
+    head_children = [n for n in graph.successors(node_idx) if graph.nodes[n]["arc_label"]["old"] == new_head_label]
+    if head_children:
+        graph.remove_edge(parent[0], node_idx)
+        graph.remove_edge(node_idx, head_children[0])
+        graph.add_edge(parent[0], head_children[0], label="obl")
+        graph.add_edge(head_children[0], node_idx, label="case")
+
+
 def convert_labels(graph, node_idx):
     node = graph.nodes[node_idx]
+    parent_idx = list(graph.predecessors(node_idx))
+    entire_row_changes = CONVERSION_TABLE["labels_with_features_changed"]
+    if node["arc_label"]["old"] == "prepmod":
+        reverse_arc_direction(graph, node_idx, "pobj")
     old_label = node['arc_label']['old']
+    if parent_idx:
+        parent = graph.nodes[parent_idx[0]]
+        if old_label == "neg":
+            node["arc_label"]["new"] = "det" if parent["pos"]["old"] == "NN" else "advmod"
+
     if old_label in LABELS.keys():
-        node['pos']['new'] = LABELS[old_label]
+        node['arc_label']['new'] = LABELS[old_label]
+    elif old_label in entire_row_changes.keys():
+        node["arc_label"]["new"] = entire_row_changes[old_label]["label"]
+        node["pos"]["new"] = entire_row_changes[old_label]["pos"]
+        node["features"]["new"] = compose_features(old_label, node, entire_row_changes)
 
 
 def convert_features(graph, node_idx):
@@ -46,6 +78,17 @@ def convert_features(graph, node_idx):
             specific_feats_conversions(node)
 
 
+def specific_feats_conversions(node):
+    if node["arc_label"]["old"] == "posspmod":
+        node["features"]["new"] = 'Case=Gen'
+    elif node['pos']['old'] == "PRP":
+        if node['arc_label']['old'] == "subj":
+            node["features"]["new"] += "|PronType=Prs"
+        elif node['arc_label']['old'] == "det":
+            node["features"]["new"] += "|PronType=Dem"
+    return node
+
+
 def convert_pos(graph, node_idx):
     node = graph.nodes[node_idx]
     if node['pos']['old'] == 'S_PRN':
@@ -64,139 +107,119 @@ def convert_pos(graph, node_idx):
         else:
             if old_pos:
                 node['pos']['new'] = ENTIRE_LINE[old_pos]["pos"]
-                replacement_feats = ENTIRE_LINE[old_pos]["feats"]
-                if replacement_feats["old"] == "+feats+":
-                    features = replacement_feats["new"][0] + node['features']['old'] + replacement_feats["new"][1]
-                elif replacement_feats["old"] == "feats+":
-                    features = node['features']['old'] + replacement_feats["new"][0]
-                elif replacement_feats["old"] == "+feats":
-                    features = replacement_feats["new"][0] + node['features']['old']
-                else:
-                    features = replacement_feats["new"][0]
-                node['features']['new'] = features
+                node['features']['new'] = compose_features(old_pos, node, ENTIRE_LINE)
                 node['arc_label']['new'] = ENTIRE_LINE[old_pos].get("arc_label") or ""
     return graph
 
 
+def compose_features(key, node, map):
+    replacement_feats = map[key]["feats"]
+    if replacement_feats["old"] == "+feats+":
+        features = replacement_feats["new"][0] + node['features']['old'] + replacement_feats["new"][1]
+    elif replacement_feats["old"] == "feats+":
+        features = node['features']['old'] + replacement_feats["new"][0]
+    elif replacement_feats["old"] == "+feats":
+        features = replacement_feats["new"][0] + node['features']['old']
+    else:
+        features = replacement_feats["new"][0]
+    return features
+
+
 def convert_graph(graph: nx.DiGraph) -> nx.DiGraph:
     for node_idx in graph.nodes:
-        node = graph.nodes[node_idx]
-        if node:
-            convert_pos(graph, node_idx)
-            convert_features(graph, node_idx)
-
+        convert_pos(graph, node_idx)
+        convert_features(graph, node_idx)
+        convert_labels(graph, node_idx)
     return graph
 
 
-def add_empty_nodes_if_necessary(graph, i, word, pos, lemma, features, arc_label, parent):
+def add_functional_nodes_if_necessary(graph, node_idx):
+    node = graph.nodes[node_idx]
+    pos = node["pos"]["old"]
+    features = node["features"]["old"]
     offset = 0
     if "suf_" in features:
         suffix_feats = "|".join([x for x in features.split("|") if 'suf' in x])
         clean_suffix_feats = "|".join([x.replace("suf_", "") for x in features.split("|") if 'suf' in x])
         if pos in ["NN", "NN_S_PP", "S_PP"]:
             graph.add_node(
-                i+1,
+                node_idx+1,
                 word={"new": "_של_", "old": ""},
                 pos={"new": "ADP", "old": ""},
                 lemma={"new": "_של_", "old": ""},
                 features={"new": "_", "old": features},
-                arc_label={"new": "case:gen", "old": ""},
-                parent=i+2,
+                arc_label={"new": "case:gen", "old": "case:gen"},
+                parent=node_idx+2,
             )
             graph.add_node(
-                i+2,
+                node_idx+2,
                 word={"new": PRONOUNS[suffix_feats], "old": ""},
                 pos={"new": "PRON", "old": ""},
                 lemma={"new": "הוא", "old": ""},
                 features={"new": f"Case=Gen|{clean_suffix_feats}|PronType=Prs", "old": ""},
-                arc_label={"new": "nmod:poss", "old": ""},
-                parent=i,
+                arc_label={"new": "nmod:poss", "old": "nmod:poss"},
+                parent=node_idx,
             )
             offset += 2
         elif pos in ["DTT", "DT"]: # כולנו
             # add another row of הוא
             graph.add_node(
-                i+1,
+                node_idx+1,
                 word={"new": PRONOUNS[suffix_feats], "old": ""},
                 pos={"new": "PRON", "old": ""},
                 lemma={"new": "הוא", "old": ""},
                 features={"new": f"Case=Gen|{clean_suffix_feats}|PronType=Prs", "old": features},
-                arc_label={"new": "nmod:poss", "old": ""},
-                parent=i,
+                arc_label={"new": "nmod:poss", "old": "nmod:poss"},
+                parent=node_idx,
             )
             offset += 1
     return offset
 
 
-def convert_sentence_to_graph(dep_tree) -> nx.DiGraph:
-    graph = nx.DiGraph()
+def add_edges(graph, parents_traceback):
+    for x in range(len(graph.nodes)):
+        node = graph.nodes[x]
+        if node["parent"] == -1:
+            continue
+        elif node["parent"] in parents_traceback.keys():
+            parent = parents_traceback[node["parent"]]
+            graph.add_edge(u_of_edge=parent, v_of_edge=x, label=node["arc_label"]["old"])
+        else:
+            graph.add_edge(u_of_edge=node["parent"], v_of_edge=x, label=node["arc_label"]["old"])
+
+
+def add_nodes(graph, dep_tree):
     offset = 0
     parents_traceback = defaultdict(int)
     for i, row in dep_tree.iterrows():
-        word = row["word"]
-        pos = row['pos']
-        lemma = row['lemma']
-        features = row["empty"]
-        arc_label = row["dependency_part"]
-        parent = int(row["dependency_arc"])
-        if parent != 0 and offset > 0:  # do not add offset if parent is 0 (root)
+        parent = int(row["dependency_arc"])-1
+        if parent >= 0 and offset > 0:  # do not add offset if parent is 0 (root)
             parents_traceback[parent] = parent + offset
         graph.add_node(i+offset,
-                       word={"new": "", "old": word},
-                       pos={"new": "", "old": pos},
-                       lemma={"new": "", "old": lemma},
-                       features={"new": "", "old": features},
-                       arc_label={"new": "", "old": arc_label},
+                       word={"new": "", "old": row["word"]},
+                       pos={"new": "", "old": row['pos']},
+                       lemma={"new": "", "old": row['lemma']},
+                       features={"new": "", "old": row["empty"]},
+                       arc_label={"new": "", "old": row["dependency_part"]},
                        parent=parent,
                        )
-        offset += add_empty_nodes_if_necessary(graph, i, word, pos, lemma, features, arc_label, parent)
+        offset += add_functional_nodes_if_necessary(graph, i+offset)
+    return parents_traceback
 
-    for x in range(len(graph.nodes)):
-        node = graph.nodes[x]
-        if node["parent"] == 0:
-            # print(f"parent {parent}, child {i} word {word} label {arc_label}")
-            graph.add_edge(u_of_edge=0, v_of_edge=x + 1, label='root')
-        elif node["parent"] in parents_traceback.keys():
-            # print(f"parent {parent}, child {i} word {word} label {arc_label}")
-            graph.add_edge(u_of_edge=parents_traceback[node["parent"]], v_of_edge=x + 1, label=node["arc_label"]["old"])
-        elif node["arc_label"]["old"] in ["prepmod", "pobj"]:
-            if node["arc_label"]["old"] == "prepmod":
-                pobj_node_idx, new_pobj_parent = reverse_arc_direction(graph, x, new_head_label="pobj")
-                graph.add_edge(u_of_edge=new_pobj_parent, v_of_edge=pobj_node_idx, label="obl")
-                graph.add_edge(u_of_edge=pobj_node_idx, v_of_edge=x+1, label="case")
-        else:
-            graph.add_edge(u_of_edge=node["parent"], v_of_edge=x + 1, label=node["arc_label"]["old"])
+
+def convert_sentence_to_graph(dep_tree) -> nx.DiGraph:
+    graph = nx.DiGraph()
+    parents_traceback = add_nodes(graph, dep_tree)
+    add_edges(graph, parents_traceback)
     convert_graph(graph)
     return graph
-
-
-def reverse_arc_direction(graph, current_head_idx, new_head_label):
-    current_head_node = graph.nodes[current_head_idx]
-    for idx in range(current_head_idx, len(graph.nodes)):
-        node = graph.nodes[idx]
-        if node["arc_label"]["old"] == new_head_label:
-            if node["parent"] == current_head_idx+1:
-                required_node_idx = idx+1
-                new_required_parent = current_head_node["parent"]
-                return required_node_idx, new_required_parent
-
-
-def specific_feats_conversions(node):
-    if node["arc_label"]["old"] == "posspmod":
-        node["features"]["new"] = 'Case=Gen'
-    elif node['pos']['old'] == "PRP":
-        if node['arc_label']['old'] == "subj":
-            node["features"]["new"] += "|PronType=Prs"
-        elif node['arc_label']['old'] == "det":
-            node["features"]["new"] += "|PronType=Dem"
-    return node
 
 
 if __name__ == '__main__':
     from yap_api import YapApi
     import matplotlib.pyplot as plt
-
-    text = "גנן גידל דגן בגן"
+    # text = "הם מצאו דרך לעשות קופה"
+    text = "גנן גידל דגן בגנו"
     ip = '127.0.0.1:8000'
     yap = YapApi()
     parsed_text = yap.run(text, ip)
